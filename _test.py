e633d915d7f7249fb87bbfc1924d8c7873b80f85
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-11-02 10:18:56 +0300

    deving q

--- _test.py
+++ _test.py
@@ -1,5 +1,6 @@
-from utils.time_extractor import lunch_time
+from utils.time_extractor import lunch_time, _time_diff
 
 
-ret = lunch_time(5)
+# ret = lunch_time(5)
+ret = _time_diff(8)
 print(ret)
--- utils/time_extractor.py
+++ utils/time_extractor.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+
 def time_extractor(str_time: str):
     """default time definition: 10_min/10_MIN"""
 
@@ -12,12 +15,20 @@ def time_extractor(str_time: str):
     return eval(f"{time_int} * {time_definitions[time_str.lower()]}")
 
 
-def lunch_time(session_time: int):
-    import math
+def _time_diff(end_time):
+    _now = datetime.now()
+    ret = end_time * 60
+    _from_now = timedelta(seconds=ret)
+    now_delt = _now + _from_now
+    return now_delt
 
+
+def lunch_time(session_time: int):
     "1 hour lunch timeeeee 🍝"
 
     # get half of session time
     # give big break after half of work time
 
-    return math.ceil(session_time / 2)
+    import math
+
+    return math.ceil(session_time / 2), _time_diff(session_time)
